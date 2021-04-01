321decb01b4c8c3b27e94e47a11268eafe8a6511
wojci <wojciech.kurdybacha@gmail.com>
2021-04-01 21:19:57 +0200

    add separate class and score properties in FLR class

--- classes/flr.js
+++ classes/flr.js
@@ -4,6 +4,9 @@ class FLR {
         this.beginTime = FLRData.beginTime;
         this.endTime = FLRData.endTime;
         this.classType = FLRData.classType;
+        this.class = FLRData.classType[0]; // letter A, B, C, M or X
+        this.score = parseFloat(FLRData.classType.substring(1))
+        console.log(this.classType, ":::", this.class, this.score)
         this.peakTime = FLRData.peakTime;
         this.sourceLocation = FLRData.sourceLocation;
     }

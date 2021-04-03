c336ad12b73cb888917372a97921defa86cc1c83
wojci <wojciech.kurdybacha@gmail.com>
2021-04-03 19:07:00 +0200

    add navbar and core app styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import './App.css';
 import Home from "./components/Home"
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   
   return (
     
       <div>
+        <Navbar/>
         <Home />
       </div>
     
---
+++ client/src/components/Navbar/MenuItems.js
@@ -0,0 +1,19 @@
+const MenuItems = [
+    {
+        title: "Home",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "News",
+        url: "#",
+        cName: "nav-links"
+    },
+    {
+        title: "About",
+        url: "#",
+        cName: "nav-links"
+    },
+]
+
+export {MenuItems};
